268e472cb221bcc38c2f2132fa971202e340484b
Christian <70939559+Chrata@users.noreply.github.com>
2023-12-08 17:57:36 +0700

    add database

---
+++ perpustakaan.sql
@@ -0,0 +1,28 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `useradmin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `nim` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `prodi` varchar(100) NOT NULL,
+  `semester` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `handphone` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `useradmin`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`nim`);
+
+ALTER TABLE `useradmin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
